00ced3ad07aab5b0477037622a266f12b62fa38a
olelong <olelong@debian>
2022-11-24 14:58:02 +0100

    Add trying script bash for sql command

---
+++ srcs/requirements/mariadb/database.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+mysql -e "CREATE DATABASE wp;"
+mysql -e " USE wp;"
+mysql -e "CREATE_TABLE wordpress;"
+
+mysql -e "CREATE USER 'root'@'localhost' IDENTIFIED BY 'pass';"
+mysql -e "GRANT ALL PRIVILEGES ON 'wp' TO 'root'@'localhost';"
+
+
+mysql -e "CREATE USER 'user'@'localhost' IDENTIFIED BY 'pass';"
+mysql -e "GRANT ALL PRIVILEGES ON 'wp' TO 'user'@'localhost';"
+
