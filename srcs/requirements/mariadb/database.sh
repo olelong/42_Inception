c06ada83eef7141885eff634def8e6c4505eec04
Oriane LELONG <olelong@e3r1p3.clusters.42paris.fr>
2022-12-02 17:13:38 +0100

    abandon wp cli

--- srcs/requirements/mariadb/database.sh
+++ srcs/requirements/mariadb/database.sh
@@ -1,30 +1,31 @@
 #!/bin/sh
 
-<<MYSQL_SCRIPT
-CREATE DATABASE wp;
-mysql -e "CREATE DATABASE wp;"
-mysql -e " USE wp;"
-mysql -e "CREATE_TABLE wordpress;"
-
-mysql -e "CREATE USER 'root'@'localhost' IDENTIFIED BY 'pass';"
-
-mysql -e "GRANT ALL PRIVILEGES ON wp TO 'root'@'localhost';"
-
+#<<MYSQL_SCRIPT
+#CREATE DATABASE wp;
+#mysql -e "CREATE DATABASE wp;"
+#mysql -e " USE wp;"
+#mysql -e "CREATE_TABLE wordpress;"
+#mysql -e "CREATE USER 'root'@'localhost' IDENTIFIED BY 'pass';"
+#mysql -e "GRANT ALL PRIVILEGES ON wp TO 'root'@'localhost';"
 # To ask root password
-mysql -uroot -p'pass' -e "GRANT ALL PRIVILEGES ON wp TO 'root'@'localhost';"
-mysql -e "FLUSH PRIVILEGES;"
-
-
-mysql -e "CREATE USER 'user'@'localhost' IDENTIFIED BY 'pass';"
-mysql -e "GRANT ALL PRIVILEGES ON 'wp' TO 'user'@'localhost';"
-mysql -e "FLUSH PRIVILEGES;"
-
-MYSQL_SCRIPT
+#mysql -uroot -p'pass' -e "GRANT ALL PRIVILEGES ON wp TO 'root'@'localhost';"
+#mysql -e "FLUSH PRIVILEGES;"
+#mysql -e "CREATE USER 'user'@'localhost' IDENTIFIED BY 'pass';"
+#mysql -e "GRANT ALL PRIVILEGES ON 'wp' TO 'user'@'localhost';"
+#mysql -e "FLUSH PRIVILEGES;"
+#MYSQL_SCRIPT
 
 mysql << EOF
 CREATE DATABASE wp;
 USE wp;
-CREATE_TABLE
+CREATE_TABLE wordpress;
+CREATE USER 'root'@'localhost' IDENTIFIED BY 'pass';
+GRANT ALL PRIVILEGES ON wp TO 'root'@'localhost';
+GRANT ALL PRIVILEGES ON wp TO 'root'@'localhost';
+FLUSH PRIVILEGES;
+CREATE USER 'user'@'localhost' IDENTIFIED BY 'pass';
+GRANT ALL PRIVILEGES ON 'wp' TO 'user'@'localhost';
+FLUSH PRIVILEGES;
 EOF
 
 echo "MySQL user"
