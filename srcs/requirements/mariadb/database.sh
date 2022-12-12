60c01c6ffc1371b4e0b769cd2da09fc435be4fde
Oriane LELONG <olelong@e3r13p12.clusters.42paris.fr>
2022-12-12 19:05:20 +0100

    Try to solve mysql connection problem

--- srcs/requirements/mariadb/database.sh
+++ srcs/requirements/mariadb/database.sh
@@ -1,26 +1,10 @@
 #!/bin/sh
 
-#<<MYSQL_SCRIPT
-#CREATE DATABASE wp;
-#mysql -e "CREATE DATABASE wp;"
-#mysql -e " USE wp;"
-#mysql -e "CREATE_TABLE wordpress;"
-#mysql -e "CREATE USER 'root'@'localhost' IDENTIFIED BY 'pass';"
-#mysql -e "GRANT ALL PRIVILEGES ON wp TO 'root'@'localhost';"
-# To ask root password
-#mysql -uroot -p'pass' -e "GRANT ALL PRIVILEGES ON wp TO 'root'@'localhost';"
-#mysql -e "FLUSH PRIVILEGES;"
-#mysql -e "CREATE USER 'user'@'localhost' IDENTIFIED BY 'pass';"
-#mysql -e "GRANT ALL PRIVILEGES ON 'wp' TO 'user'@'localhost';"
-#mysql -e "FLUSH PRIVILEGES;"
-#MYSQL_SCRIPT
-
 mysql << EOF
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'pass';
 CREATE DATABASE wp;
 USE wp;
 CREATE_TABLE wordpress;
-CREATE USER 'root'@'localhost' IDENTIFIED BY 'pass';
-GRANT ALL PRIVILEGES ON wp TO 'root'@'localhost';
 GRANT ALL PRIVILEGES ON wp TO 'root'@'localhost';
 FLUSH PRIVILEGES;
 CREATE USER 'user'@'localhost' IDENTIFIED BY 'pass';
