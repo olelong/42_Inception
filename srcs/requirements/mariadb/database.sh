6aeb20806847b35cbfc2aa66cccf2791ca67be04
olelong <olelong@student.42.fr>
2022-12-14 23:30:22 +0100

    add comments

--- srcs/requirements/mariadb/database.sh
+++ srcs/requirements/mariadb/database.sh
@@ -14,15 +14,19 @@ FLUSH PRIVILEGES;
 EOF
 
 pkill mysqld
-mysqld
-#mysqld_safe #--user="mysql"
+mysqld_safe
 
-#CREATE DATABASE wp;
-#USE wp;
-#CREATE_TABLE wordpress;
-#ALTER USER 'root'@'localhost' IDENTIFIED BY 'pass';
-#GRANT ALL PRIVILEGES ON wp TO 'root'@'localhost';
-#FLUSH PRIVILEGES;
-#CREATE USER 'user'@'localhost' IDENTIFIED BY 'pass';
-#GRANT ALL PRIVILEGES ON 'wp' TO 'user'@'localhost';
-#FLUSH PRIVILEGES;
+# Lancer mysqld et on redirige stderr vers un fichier null
+# ce qui permet de ne pas afficher les erreurs.
+# Le "&" permet de mettre mysqld en arriere plan.
+# Le sleep 5 est au premier plan et bloque le reste pour
+# laisser le temps a mysqld de bien s'initialiser.
+
+# Ensuite il rentre dans le mysql.
+
+# Le pkill, permet de kill tous les processus lancé par la 
+# commande en argument donc ici mysqld.
+# Car il faut quitter et relancer mysqld pour que nos
+# changements soient pris en compte.
+
+# On relance en premier plan mysqld.
